9a16fe5514b6ee9ccd4c2f2e5f98c1fa7f9c3101
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-21 20:49:59 -0300

    First commit

---
+++ src/main/kotlin/com/br/stock/control/Application.kt
@@ -0,0 +1,11 @@
+package com.br.stock.control
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/com/br/stock/control/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.stock.control
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
