19f0a48f11612c599f691c25eeddda89327f37e4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-22 18:56:29 -0300

    Set config spring security

---
+++ src/main/kotlin/com/br/stock/control/config/security/SecurityFilter.kt
@@ -0,0 +1,53 @@
+package com.br.stock.control.config.security
+
+import com.br.stock.control.config.security.service.TokenService
+import com.br.stock.control.repository.UserRepository
+import jakarta.servlet.FilterChain
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+
+@Component
+class SecurityFilter(
+    private val tokenService: TokenService,
+    private val userRepository: UserRepository
+): OncePerRequestFilter() {
+
+    override fun doFilterInternal(request: HttpServletRequest,response: HttpServletResponse,filterChain: FilterChain) {
+        val token: String? = this.recoverToken(request);
+
+        if (token != null) {
+            val login: String = tokenService.validateToken(token)
+
+            if (login.isEmpty()) {
+                val user: UserDetails? = userRepository.findByEmail(login);
+
+                if (user != null) {
+                    val authentication = UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        user.authorities
+                    )
+
+                    SecurityContextHolder.getContext().authentication = authentication
+                }
+
+            }
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    fun recoverToken(request: HttpServletRequest): String? {
+        val authHeader: String = request.getHeader("Authorization")
+        if (authHeader.isBlank()) {
+            return null
+        }
+
+        return authHeader.replace("Bearer ", "")
+    }
+
+}
---
+++ src/main/kotlin/com/br/stock/control/config/security/service/AuthorizationService.kt
@@ -0,0 +1,18 @@
+package com.br.stock.control.config.security.service
+
+import com.br.stock.control.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+@Service
+class AuthorizationService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(username: String): UserDetails? {
+
+        return userRepository.findByEmail(username);
+    }
+
+}
