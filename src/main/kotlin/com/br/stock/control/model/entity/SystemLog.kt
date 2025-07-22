1a197497a6d02f1fe5f843621bfa2f1be674dacc
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-22 18:57:49 -0300

    Fix something

--- src/main/kotlin/com/br/stock/control/model/entity/SystemLog.kt
+++ src/main/kotlin/com/br/stock/control/model/entity/SystemLog.kt
@@ -3,7 +3,6 @@ package com.br.stock.control.model.entity
 import com.br.stock.control.model.enum.EntityTypeEnum
 import com.br.stock.control.model.enum.EventTypeEnum
 import com.br.stock.control.model.enum.LevelEnum
-import org.bson.types.ObjectId
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
@@ -14,14 +13,14 @@ import java.time.LocalDateTime
 @Document(collection = "logs")
 data class SystemLog(
     @Id
-    var id: ObjectId,
+    var id: String,
     var timestamp: LocalDateTime,
     var level: LevelEnum,
     var message: String,
     var eventType: EventTypeEnum,
     var entityType: EntityTypeEnum,
-    var entityId: ObjectId,
-    var userId: ObjectId,
+    var entityId: String,
+    var userId: String,
     var ipAddress: String,
     var details: Map<String, Any>,
     @Version
