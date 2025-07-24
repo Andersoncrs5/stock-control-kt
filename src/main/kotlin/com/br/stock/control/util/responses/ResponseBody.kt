1e18322c5de60d0214628350409d9c7a28624c2b
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-24 19:30:16 -0300

    New ResponseBody.kt

---
+++ src/main/kotlin/com/br/stock/control/util/responses/ResponseBody.kt
@@ -0,0 +1,12 @@
+package com.br.stock.control.util.responses
+
+import java.time.LocalDateTime
+
+data class ResponseBody<T>(
+    val timestamp: LocalDateTime,
+    val message: String?,
+    val path: String?,
+    val method: String?,
+    val body: T?
+) {
+}
