234dd835d6a0f58a45ec7228f1acf3538b06c7ae
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-24 19:23:55 -0300

    Facades to services

---
+++ src/main/kotlin/com/br/stock/control/util/facades/FacadesServices.kt
@@ -0,0 +1,18 @@
+package com.br.stock.control.util.facades
+
+import com.br.stock.control.config.security.service.TokenService
+import com.br.stock.control.service.ProductService
+import com.br.stock.control.service.RedisService
+import com.br.stock.control.service.UserService
+import com.br.stock.control.service.WareHouseService
+import org.springframework.stereotype.Service
+
+@Service
+class FacadesServices(
+    val userService: UserService,
+    val productService: ProductService,
+    val wareHouseService: WareHouseService,
+    val tokenService: TokenService,
+    val redisService: RedisService
+) {
+}
