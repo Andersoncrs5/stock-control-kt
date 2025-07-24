fba44d0d832c257cc89f1a4657d2d2190720d692
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-24 19:32:40 -0300

    Fix something

--- src/main/kotlin/com/br/stock/control/repository/WarehouseRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/WarehouseRepository.kt
@@ -1,7 +1,8 @@
 package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.Warehouse
+import com.br.stock.control.util.filters.warehouse.WareHouseCustomRepository
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface WarehouseRepository: MongoRepository<Warehouse, String> {
+interface WarehouseRepository: MongoRepository<Warehouse, String>, WareHouseCustomRepository {
 }
