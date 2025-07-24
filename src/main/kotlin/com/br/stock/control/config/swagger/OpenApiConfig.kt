97e10adf0c6f1f32f5d9a5fb85904d9647121d36
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-24 19:28:37 -0300

    Set config swagger

---
+++ src/main/kotlin/com/br/stock/control/config/swagger/OpenApiConfig.kt
@@ -0,0 +1,23 @@
+package com.br.stock.control.config.swagger
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@OpenAPIDefinition(
+    info = Info(title = "Api to control of stock", version = "1.0", description = "API RESTful"),
+    security = []
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer",
+    `in` = SecuritySchemeIn.HEADER,
+    description = "Token JWT of authentication"
+)
+class OpenApiConfig {}
