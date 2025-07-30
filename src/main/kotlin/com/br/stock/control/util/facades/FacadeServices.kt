027c6664fe8e98121dd7a2e34eef96500c0b5b40
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-29 21:08:22 -0300

    new reference to CryptoService.kt

--- src/main/kotlin/com/br/stock/control/util/facades/FacadeServices.kt
+++ src/main/kotlin/com/br/stock/control/util/facades/FacadeServices.kt
@@ -1,5 +1,6 @@
 package com.br.stock.control.util.facades
 
+import com.br.stock.control.config.security.service.CryptoService
 import com.br.stock.control.config.security.service.TokenService
 import com.br.stock.control.service.ProductService
 import com.br.stock.control.service.RedisService
@@ -13,6 +14,7 @@ class FacadeServices(
     val productService: ProductService,
     val wareHouseService: WareHouseService,
     val tokenService: TokenService,
-    val redisService: RedisService
+    val redisService: RedisService,
+    val cryptoService: CryptoService
 ) {
 }
