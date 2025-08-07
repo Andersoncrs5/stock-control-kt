d3901fc2f6a7f861e9880212219e0876b221c7af
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-07 20:52:37 -0300

    Added method create

--- src/main/kotlin/com/br/stock/control/controller/ProductController.kt
+++ src/main/kotlin/com/br/stock/control/controller/ProductController.kt
@@ -3,10 +3,12 @@ package com.br.stock.control.controller
 import com.br.stock.control.model.entity.Product
 import com.br.stock.control.util.facades.FacadeMappers
 import com.br.stock.control.util.facades.FacadeServices
+import com.br.stock.control.util.mappers.product.CreateProductMapper
 import com.br.stock.control.util.responses.ResponseBody
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -26,8 +28,7 @@ class ProductController(
         if (id.isBlank()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                 ResponseBody<Product>(
-                    LocalDateTime.now(),
-                    "Id is required", request.requestURI,
+                    LocalDateTime.now(), "Id is required", request.requestURI,
                     request.method, null
                 )
             )
@@ -38,8 +39,7 @@ class ProductController(
         if (product == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 ResponseBody<Product>(
-                    LocalDateTime.now(),
-                    "Product not found", request.requestURI,
+                    LocalDateTime.now(), "Product not found", request.requestURI,
                     request.method, null
                 )
             )
@@ -47,13 +47,21 @@ class ProductController(
 
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseBody<Product>(
-                LocalDateTime.now(),
-                "Product founded", request.requestURI,
+                LocalDateTime.now(), "Product founded", request.requestURI,
                 request.method, product
             )
         )
     }
 
+    @PostMapping("/{categoryId}")
+    @SecurityRequirement(name = "bearerAuth")
+    @RateLimiter(name = "createApiRateLimiter")
+    fun create(
+        @PathVariable categoryId: String,
+        @Valid @RequestBody dto: CreateProductMapper,
+        request: HttpServletRequest
+    ) {
 
+    }
 
 }
