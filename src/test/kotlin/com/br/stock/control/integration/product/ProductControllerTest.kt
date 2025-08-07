3f87f32b380da1c4f0c76c0bc4b76c91bed2bb26
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-07 20:55:31 -0300

    new test to product controller

---
+++ src/test/kotlin/com/br/stock/control/integration/product/ProductControllerTest.kt
@@ -0,0 +1,25 @@
+package com.br.stock.control.integration.product
+
+import com.br.stock.control.util.facades.FacadeRepository
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    private lateinit var facadeRepository: FacadeRepository
+
+    private val url: String = "/v1/product"
+
+
+}
