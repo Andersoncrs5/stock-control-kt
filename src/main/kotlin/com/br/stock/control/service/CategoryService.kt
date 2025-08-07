4d89a05ac681e7a113d7e7c48d3e62dda50c1694
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-07 20:54:00 -0300

    Added new caegoryService

---
+++ src/main/kotlin/com/br/stock/control/service/CategoryService.kt
@@ -0,0 +1,64 @@
+package com.br.stock.control.service
+
+import com.br.stock.control.model.entity.Category
+import com.br.stock.control.repository.CategoryRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.Optional
+
+@Service
+class CategoryService(
+    private val repository: CategoryRepository
+) {
+    private val logger = LoggerFactory.getLogger(CategoryService::class.java)
+
+    @Transactional(readOnly = true)
+    fun get(id: String): Optional<Category> {
+        logger.debug("Getting category by id $id")
+        val category: Optional<Category> = this.repository.findById(id)
+        logger.debug("Returning category")
+        return category
+    }
+
+    @Transactional
+    fun delete(category: Category) {
+        logger.debug("Deleting category...")
+        this.repository.delete(category)
+        logger.debug("Category deleted")
+    }
+
+    @Transactional(readOnly = true)
+    fun getByName(name: String): Optional<Category> {
+        logger.debug("Getting category by name $name")
+        val category: Optional<Category> = this.repository.findByName(name)
+        logger.debug("Returning category search by name")
+        return category
+    }
+
+    @Transactional
+    fun save(category: Category): Category {
+        logger.debug("Saving category...")
+        val save = this.repository.save(category)
+        logger.debug("Category saved")
+        return save
+    }
+
+    @Transactional
+    fun deleteMany(ids: List<String>) {
+        logger.debug("Deleting many categories...")
+        this.repository.deleteAllById(ids)
+        logger.debug("Categories many deleted")
+    }
+
+    @Transactional
+    fun changeStatusActive(category: Category): Category  {
+        logger.debug("Changing status..")
+        category.active = !category.active
+        logger.debug("Saving category after changed...")
+        val save = this.repository.save(category)
+        logger.debug("Category saved after changed")
+        return save
+    }
+
+}
