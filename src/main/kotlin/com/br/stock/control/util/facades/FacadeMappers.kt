9d85044a3744b690d6395c9eca34a81bbde90586
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-07 20:54:14 -0300

    added new mapper

--- src/main/kotlin/com/br/stock/control/util/facades/FacadeMappers.kt
+++ src/main/kotlin/com/br/stock/control/util/facades/FacadeMappers.kt
@@ -1,10 +1,12 @@
 package com.br.stock.control.util.facades
 
+import com.br.stock.control.util.mappers.product.CreateProductMapper
 import com.br.stock.control.util.mappers.user.UserDTOMapper
 import org.springframework.stereotype.Service
 
 @Service
 class FacadeMappers(
-    val userDTOMapper: UserDTOMapper
+    val userDTOMapper: UserDTOMapper,
+    val createProductMapper: CreateProductMapper
 ) {
 }
