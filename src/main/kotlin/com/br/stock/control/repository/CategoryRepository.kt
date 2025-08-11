73dddd7e3e10ec4250688a1f381bf97e62551ef7
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-11 16:00:27 -0300

    Add extends custom repository

--- src/main/kotlin/com/br/stock/control/repository/CategoryRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/CategoryRepository.kt
@@ -1,10 +1,11 @@
 package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.Category
+import com.br.stock.control.util.filters.category.CategoryCustomRepository
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.Optional
 
-interface CategoryRepository : MongoRepository<Category, String> {
+interface CategoryRepository : MongoRepository<Category, String>, CategoryCustomRepository {
     fun findByName(name: String): Optional<Category>
 }
