88c2cebee11fd9d8f22fb64a862abcc6cdc38b44
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-12 20:50:56 -0300

    New dto to category

---
+++ src/main/kotlin/com/br/stock/control/model/dto/category/CreateCategoryDTO.kt
@@ -0,0 +1,8 @@
+package com.br.stock.control.model.dto.category
+
+import com.br.stock.control.model.enum.TypeAddressEnum
+
+class CreateCategoryDTO (
+    var name: String = "",
+    var description: String? = "",
+)
