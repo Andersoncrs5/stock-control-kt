c717b1d3723ac9f811585c26b9f50f180cbb61b4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-12 17:12:10 -0300

    New service to Address

---
+++ src/main/kotlin/com/br/stock/control/service/AddressService.kt
@@ -0,0 +1,75 @@
+package com.br.stock.control.service
+
+import com.br.stock.control.model.entity.Address
+import com.br.stock.control.repository.AddressRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.Optional
+
+@Service
+class AddressService(
+    private val repository: AddressRepository
+) {
+    private val logger = LoggerFactory.getLogger(AddressService::class.java)
+
+    @Transactional(readOnly = true)
+    fun get(id: String): Optional<Address> {
+        logger.debug("Getting address by id")
+        val opt: Optional<Address> = this.repository.findById(id)
+        logger.debug("Returning address")
+        return opt
+    }
+
+    @Transactional
+    fun delete(address: Address) {
+        logger.debug("Deleting address")
+        this.repository.delete(address)
+        logger.debug("Address deleted")
+    }
+
+    @Transactional
+    fun deleteMany(ids: List<String>) {
+        logger.debug("Deleting many address")
+        this.repository.deleteAllById(ids)
+        logger.debug("Address many deleted")
+    }
+
+    @Transactional
+    fun save(address: Address): Address {
+        logger.debug("Saving address")
+        val save: Address = this.repository.save(address)
+        logger.debug("Address save")
+        return save
+    }
+
+    fun mergeAddress(original: Address, toMerge: Address): Address {
+        logger.debug("Merging addresses...")
+
+        original.street = toMerge.street
+        original.number = toMerge.number
+        original.complement = toMerge.complement
+        original.neighborhood = toMerge.neighborhood
+        original.city = toMerge.city
+        original.state = toMerge.state
+        original.zipCode = toMerge.zipCode
+        original.country = toMerge.country
+        original.referencePoint = toMerge.referencePoint
+        original.latitude = toMerge.latitude
+        original.longitude = toMerge.longitude
+        original.type = toMerge.type
+
+        logger.debug("Addresses merged")
+        return original
+    }
+
+    @Transactional
+    fun changeStatusActive(address: Address): Address {
+        logger.debug("Change status address active...")
+        address.isActive = !address.isActive
+        val save = this.repository.save(address)
+        logger.debug("Address active changed")
+        return save
+    }
+
+}
