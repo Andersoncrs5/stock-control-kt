fcc161c3cfadcd9a5a1933d000f789ff80b75eb6
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-13 19:03:07 -0300

    set logs

--- src/main/kotlin/com/br/stock/control/controller/CategoryController.kt
+++ src/main/kotlin/com/br/stock/control/controller/CategoryController.kt
@@ -10,6 +10,7 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -30,11 +31,15 @@ class CategoryController(
     private val facadesMappers: FacadeMappers
 ) {
 
+    private val logger = LoggerFactory.getLogger(CategoryController::class.java)
+
     @GetMapping("/{id}")
     @RateLimiter(name = "readApiRateLimiter")
     @SecurityRequirement(name = "bearerAuth")
     fun get(@PathVariable id: String, request: HttpServletRequest): ResponseEntity<ResponseBody<Category>> {
+        logger.debug("Getting category by id $id")
         if (id.isBlank()) {
+            logger.debug("Id came null")
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                 ResponseBody(
                     LocalDateTime.now(), "Id is required!",
@@ -43,9 +48,11 @@ class CategoryController(
             )
         }
 
+        logger.debug("Searching in the db")
         val category: Optional<Category> = this.facade.category.get(id)
 
         if (category.isEmpty) {
+            logger.debug("Category  not found")
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 ResponseBody(
                     LocalDateTime.now(), "Category not found!",
@@ -54,6 +61,7 @@ class CategoryController(
             )
         }
 
+        logger.debug("Category found")
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseBody(
                 LocalDateTime.now(), "Category found!",
@@ -69,12 +77,25 @@ class CategoryController(
         @Valid @RequestBody dto: CreateCategoryDTO,
         request: HttpServletRequest
     ): ResponseEntity<ResponseBody<Category>> {
+        logger.debug("Creating new category")
         val category: Category = this.facadesMappers.createCategoryMapper.toCategory(dto)
+
+        val byName: Optional<Category> = this.facade.category.getByName(category.name)
+
+        if (byName.isPresent) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Category name already exists!",
+                    request.requestURI, request.method, null
+                )
+            )
+        }
 
         category.id = UUID.randomUUID().toString()
         val result: Category = this.facade.category.save(category)
 
-        return ResponseEntity.status(HttpStatus.OK).body(
+        logger.debug("Returning neew category")
+        return ResponseEntity.status(HttpStatus.CREATED).body(
             ResponseBody(
                 LocalDateTime.now(), "Category created!",
                 request.requestURI, request.method, result
@@ -86,13 +107,13 @@ class CategoryController(
     @RateLimiter(name = "readApiRateLimiter")
     @SecurityRequirement(name = "bearerAuth")
     fun getAll(request: HttpServletRequest): ResponseEntity<ResponseBody<List<Category>>> {
-        val cached = facade.redisService.get<List<Category>>("categories")
+        val cached: List<Category>? = facade.redisService.get<List<Category>>("categories")
 
-        val categories = cached ?: facade.category.getAll().also {
+        val categories: List<Category> = cached ?: facade.category.getAll().also {
             facade.redisService.set("categories", it, Duration.ofMinutes(10))
         }
 
-        return ResponseEntity.ok(
+        return ResponseEntity.status(HttpStatus.OK).body(
             ResponseBody(
                 timestamp = LocalDateTime.now(),
                 message = "Categories found!",
