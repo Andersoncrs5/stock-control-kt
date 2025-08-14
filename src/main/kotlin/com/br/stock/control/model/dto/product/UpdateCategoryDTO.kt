cb83d0b31718e9c7474206dba3235aa3a9e17bd8
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-14 20:19:34 -0300

    Added update dto to Category

---
+++ src/main/kotlin/com/br/stock/control/model/dto/product/UpdateCategoryDTO.kt
@@ -0,0 +1,17 @@
+package com.br.stock.control.model.dto.product
+
+import com.br.stock.control.model.enum.UnitOfMeasureEnum
+import java.math.BigDecimal
+
+data class UpdateCategoryDTO(
+    var name: String = "",
+    var description: String = "",
+    var sku: String = "",
+    var barcode: String = "",
+    var unitOfMeasure: UnitOfMeasureEnum? = null,
+    var price: BigDecimal? = null,
+    var cost: BigDecimal? = null,
+    var imageUrl: String = "",
+    var minStockLevel: Int = 0,
+    var maxStockLevel: Int = 0
+)
