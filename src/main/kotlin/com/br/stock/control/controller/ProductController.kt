c56036060a04c7678c794ce6227ea08828e1495e
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-14 17:51:50 -0300

    More methods

--- src/main/kotlin/com/br/stock/control/controller/ProductController.kt
+++ src/main/kotlin/com/br/stock/control/controller/ProductController.kt
@@ -1,5 +1,7 @@
 package com.br.stock.control.controller
 
+import com.br.stock.control.model.dto.product.CreateProductDTO
+import com.br.stock.control.model.entity.Category
 import com.br.stock.control.model.entity.Product
 import com.br.stock.control.util.facades.FacadeMappers
 import com.br.stock.control.util.facades.FacadeServices
@@ -9,10 +11,13 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
+import java.util.Optional
+import java.util.UUID
 
 @RestController
 @RequestMapping("/v1/product")
@@ -20,6 +25,8 @@ class ProductController(
     private val facades: FacadeServices,
     private val facadeMappers: FacadeMappers
 ) {
+
+    private val logger = LoggerFactory.getLogger(ProductController::class.java)
 
     @GetMapping("/{id}")
     @SecurityRequirement(name = "bearerAuth")
@@ -58,10 +65,43 @@ class ProductController(
     @RateLimiter(name = "createApiRateLimiter")
     fun create(
         @PathVariable categoryId: String,
-        @Valid @RequestBody dto: CreateProductMapper,
+        @Valid @RequestBody dto: CreateProductDTO,
         request: HttpServletRequest
-    ) {
+    ): ResponseEntity<ResponseBody<Product>> {
+        if (categoryId.isBlank()) {
+            logger.debug("Id came null")
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Id is required!",
+                    request.requestURI, request.method, null
+                )
+            )
+        }
 
+        val category: Optional<Category> = this.facades.category.get(categoryId)
+
+        if (category.isEmpty) {
+            logger.debug("Category  not found")
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Category not found!",
+                    request.requestURI, request.method, null
+                )
+            )
+        }
+
+        val product: Product = this.facadeMappers.createProductMapper.toProduct(dto)
+        product.id = UUID.randomUUID().toString()
+        product.categoryId = category.get().id
+
+        val result: Product = this.facades.productService.save(product)
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            ResponseBody(LocalDateTime.now(),
+                "Product created", request.requestURI,
+                request.method, result
+            )
+        )
     }
 
 }
