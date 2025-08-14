9c355f002decc8771cf7aa1fb6ad0869bfaff2e4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-14 19:52:25 -0300

    Added new method findBySku and findByBarcode

--- src/main/kotlin/com/br/stock/control/repository/ProductRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/ProductRepository.kt
@@ -3,6 +3,9 @@ package com.br.stock.control.repository
 import com.br.stock.control.model.entity.Product
 import com.br.stock.control.util.filters.products.ProductCustomRepository
 import org.springframework.data.mongodb.repository.MongoRepository
+import java.util.Optional
 
 interface ProductRepository: MongoRepository<Product, String>, ProductCustomRepository {
+    fun findBySku(sku: String): Optional<Product>
+    fun findByBarcode(barcode: String): Optional<Product>
 }
