4637aecc61df22602c0f2c0afcd118669507df38
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-16 19:03:11 -0300

    Added Address Controller test

---
+++ src/test/kotlin/com/br/stock/control/integration/address/AddressControllerTest.kt
@@ -0,0 +1,30 @@
+package com.br.stock.control.integration.address
+
+import com.br.stock.control.util.facades.FacadeRepository
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AddressControllerTest {
+    @Autowired private lateinit var mockMvc: MockMvc
+
+    @Autowired private lateinit var objectMapper: ObjectMapper
+
+    @Autowired private lateinit var facadeRepository: FacadeRepository
+
+    private val url: String = "/v1/address"
+
+    @BeforeEach fun setup() {
+        this.facadeRepository.userRepository.deleteAll()
+        this.facadeRepository.wareHouseRepository.deleteAll()
+        this.facadeRepository.addressRepository.deleteAll()
+    }
+
+
+
+}
