ceedfcda431a65cdd3130e743fd3da71981391e7
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-16 15:19:35 -0300

    Add dto to warehouse

---
+++ src/main/kotlin/com/br/stock/control/model/dto/warehouse/UpdateWareDTO.kt
@@ -0,0 +1,13 @@
+package com.br.stock.control.model.dto.warehouse
+
+import com.br.stock.control.model.enum.WareHouseEnum
+
+data class UpdateWareDTO(
+    var name: String = "",
+    var description: String = "",
+    var addressId: String = "",
+    var responsibleUserId: String = "",
+    var amount: Long = 0L,
+    var capacityCubicMeters: Double = 0.0,
+    var type: WareHouseEnum = WareHouseEnum.NONE
+)
