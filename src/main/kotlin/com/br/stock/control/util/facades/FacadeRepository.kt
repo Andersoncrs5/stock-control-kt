2da07b111ffbb18bc9a0417d35affe2a322919b4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-16 15:19:58 -0300

    New reference to wareHouseRepository

--- src/main/kotlin/com/br/stock/control/util/facades/FacadeRepository.kt
+++ src/main/kotlin/com/br/stock/control/util/facades/FacadeRepository.kt
@@ -3,12 +3,14 @@ package com.br.stock.control.util.facades
 import com.br.stock.control.repository.CategoryRepository
 import com.br.stock.control.repository.ProductRepository
 import com.br.stock.control.repository.UserRepository
+import com.br.stock.control.repository.WarehouseRepository
 import org.springframework.stereotype.Service
 
 @Service
 class FacadeRepository(
     val userRepository: UserRepository,
     val productRepository: ProductRepository,
-    val categoryRepository: CategoryRepository
+    val categoryRepository: CategoryRepository,
+    val wareHouseRepository: WarehouseRepository
 ) {
 }
