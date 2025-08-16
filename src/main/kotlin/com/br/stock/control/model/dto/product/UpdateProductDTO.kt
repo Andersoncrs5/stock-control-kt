808294b6898e16a422f88cfe03655e0259653e79
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-15 21:22:09 -0300

    fix something

--- src/main/kotlin/com/br/stock/control/model/dto/product/UpdateProductDTO.kt
+++ src/main/kotlin/com/br/stock/control/model/dto/product/UpdateProductDTO.kt
@@ -3,15 +3,16 @@ package com.br.stock.control.model.dto.product
 import com.br.stock.control.model.enum.UnitOfMeasureEnum
 import java.math.BigDecimal
 
-data class UpdateCategoryDTO(
+data class UpdateProductDTO(
     var name: String = "",
     var description: String = "",
     var sku: String = "",
     var barcode: String = "",
-    var unitOfMeasure: UnitOfMeasureEnum? = null,
-    var price: BigDecimal? = null,
-    var cost: BigDecimal? = null,
+    var unitOfMeasure: UnitOfMeasureEnum = UnitOfMeasureEnum.UNIT,
+    var price: BigDecimal = BigDecimal.valueOf(0.0),
+    var cost: BigDecimal = BigDecimal.valueOf(0.0),
     var imageUrl: String = "",
     var minStockLevel: Int = 0,
-    var maxStockLevel: Int = 0
+    var maxStockLevel: Int = 0,
+    var locationSpecificStock: Map<String, Int> = mapOf(),
 )
