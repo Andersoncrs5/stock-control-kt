ef7c37cdf7cd300e9a2c49cd0cd8b4c5cdf83d87
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-17 12:33:23 -0300

    New method existsByIdAndType

--- src/main/kotlin/com/br/stock/control/repository/AddressRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/AddressRepository.kt
@@ -1,8 +1,9 @@
 package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.Address
-import org.bson.types.ObjectId
+import com.br.stock.control.model.enum.TypeAddressEnum
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface AddressRepository: MongoRepository<Address, String> {
+    fun existsByIdAndType(id: String, type: TypeAddressEnum): Boolean
 }
