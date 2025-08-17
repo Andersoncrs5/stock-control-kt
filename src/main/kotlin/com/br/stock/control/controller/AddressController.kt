9fc8a37bac9d0d870565c8b003ed2b60b8c653f7
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-17 12:32:18 -0300

    Added method delete, update, changeStatus

--- src/main/kotlin/com/br/stock/control/controller/AddressController.kt
+++ src/main/kotlin/com/br/stock/control/controller/AddressController.kt
@@ -1,5 +1,6 @@
 package com.br.stock.control.controller
 
+import com.br.stock.control.model.dto.address.UpdateAddressDTO
 import com.br.stock.control.model.entity.Address
 import com.br.stock.control.util.facades.FacadeMappers
 import com.br.stock.control.util.facades.FacadeServices
@@ -7,9 +8,11 @@ import com.br.stock.control.util.responses.ResponseBody
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
 import java.time.LocalDateTime
 import java.util.Optional
 
@@ -53,4 +56,121 @@ class AddressController(
             )
         )
     }
+
+    @DeleteMapping("/{id}")
+    @RateLimiter(name = "deleteApiRateLimiter")
+    @SecurityRequirement(name = "bearerAuth")
+    fun delete(
+        @PathVariable id: String,
+        request: HttpServletRequest
+    ): ResponseEntity<ResponseBody<Unit>> {
+        if (id.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Id is required", request.requestURI,
+                    request.method, Unit
+                )
+            )
+        }
+
+        val optional: Optional<Address> = this.facadeServices.addressService.get(id)
+
+        if (optional.isEmpty) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Address not found", request.requestURI,
+                    request.method, Unit
+                )
+            )
+        }
+
+        this.facadeServices.addressService.delete(optional.get())
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(
+            ResponseBody(
+                LocalDateTime.now(), "Address deleted", request.requestURI,
+                request.method, Unit
+            )
+        )
+    }
+
+    @PutMapping("/{id}")
+    @RateLimiter(name = "updateApiRateLimiter")
+    @SecurityRequirement(name = "bearerAuth")
+    fun update(
+        @PathVariable id: String,
+        @Valid @RequestBody dto: UpdateAddressDTO,
+        request: HttpServletRequest
+    ): ResponseEntity<ResponseBody<Address?>> {
+        if (id.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Id is required", request.requestURI,
+                    request.method, null
+                )
+            )
+        }
+
+        val optional: Optional<Address> = this.facadeServices.addressService.get(id)
+
+        if (optional.isEmpty) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Address not found", request.requestURI,
+                    request.method, null
+                )
+            )
+        }
+
+        val addressOriginal: Address = optional.get()
+        val toMerge: Address = this.facadeMappers.updateAddressMapper.toAddress(dto)
+
+        val update = this.facadeServices.addressService.update(addressOriginal, toMerge)
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseBody(
+                LocalDateTime.now(), "Address updated", request.requestURI,
+                request.method, update
+            )
+        )
+    }
+
+    @PutMapping("/{id}/status/active")
+    @RateLimiter(name = "readApiRateLimiter")
+    @SecurityRequirement(name = "bearerAuth")
+    fun changeStatus(
+        @PathVariable id: String,
+        request: HttpServletRequest
+    ): ResponseEntity<ResponseBody<Address?>> {
+        if (id.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Id is required", request.requestURI,
+                    request.method, null
+                )
+            )
+        }
+
+        val save: Optional<Address> = this.facadeServices.addressService.get(id)
+
+        if (save.isEmpty) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ResponseBody(
+                    LocalDateTime.now(), "Address not found", request.requestURI,
+                    request.method, null
+                )
+            )
+        }
+
+        val update = this.facadeServices.addressService.changeStatusActive(save.get())
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseBody(
+                LocalDateTime.now(), "Address status changed", request.requestURI,
+                request.method, update
+            )
+        )
+    }
+
+
 }
