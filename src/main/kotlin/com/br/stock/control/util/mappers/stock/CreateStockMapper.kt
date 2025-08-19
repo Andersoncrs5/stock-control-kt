c29a3677a19c7cca1d62e1766a26e83b47006464
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-18 21:37:36 -0300

    new mapper to stock and CreateStockDTO

---
+++ src/main/kotlin/com/br/stock/control/util/mappers/stock/CreateStockMapper.kt
@@ -0,0 +1,19 @@
+package com.br.stock.control.util.mappers.stock
+
+import com.br.stock.control.model.dto.stock.CreateStockDTO
+import com.br.stock.control.model.entity.Stock
+import com.github.dozermapper.core.Mapper
+import org.springframework.stereotype.Service
+
+@Service
+class CreateStockMapper(
+    private val mapper: Mapper
+) {
+    fun toDTO(stock: Stock): CreateStockDTO {
+        return this.mapper.map(stock, CreateStockDTO::class.java);
+    }
+
+    fun toStock(dto: CreateStockDTO): Stock {
+        return this.mapper.map(dto, Stock::class.java)
+    }
+}
