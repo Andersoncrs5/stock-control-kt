4ff95e7e9bce7db51f03d36f87d3e82665a0e35e
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-19 19:45:39 -0300

    change type quantity of int to long

--- src/main/kotlin/com/br/stock/control/model/dto/stock/CreateStockDTO.kt
+++ src/main/kotlin/com/br/stock/control/model/dto/stock/CreateStockDTO.kt
@@ -2,7 +2,7 @@ package com.br.stock.control.model.dto.stock
 
 class CreateStockDTO(
     var productId: String = "",
-    var quantity: Int = 0,
+    var quantity: Long = 0,
     var responsibleUserId: String = "",
     var warehouseId: String = "",
 )
