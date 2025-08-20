d9717d4645a0b8c7c2bba7d2b771fcfbc495f7dd
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-20 19:58:04 -0300

    new method existsByCnpj

--- src/main/kotlin/com/br/stock/control/repository/SupplierRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/SupplierRepository.kt
@@ -1,7 +1,9 @@
 package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.Supplier
+import com.br.stock.control.util.filters.supplier.SupplierCustomRepository
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface SupplierRepository: MongoRepository<Supplier, String> {
+interface SupplierRepository: MongoRepository<Supplier, String>, SupplierCustomRepository {
+    fun existsByCnpj(cnpj: String): Boolean
 }
