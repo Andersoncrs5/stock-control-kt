ad18c4544a5f779e81b0ed2c044ebb08372c6515
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-21 20:07:22 -0300

    Impl PurchaseOrderCustomRepository

--- src/main/kotlin/com/br/stock/control/repository/PurchaseOrderRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/PurchaseOrderRepository.kt
@@ -1,7 +1,7 @@
 package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.PurchaseOrder
+import com.br.stock.control.util.filters.purchaseOrder.PurchaseOrderCustomRepository
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface PurchaseOrderRepository: MongoRepository<PurchaseOrder, String> {
-}
+interface PurchaseOrderRepository: MongoRepository<PurchaseOrder, String>, PurchaseOrderCustomRepository
