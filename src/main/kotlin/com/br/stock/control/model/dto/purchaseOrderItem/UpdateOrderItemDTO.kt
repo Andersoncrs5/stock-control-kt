076c6ac1a949b36d7063a4d09308b503af1c8d37
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-21 21:31:31 -0300

    New dto to purchaseOrderItem

---
+++ src/main/kotlin/com/br/stock/control/model/dto/purchaseOrderItem/UpdateOrderItemDTO.kt
@@ -0,0 +1,12 @@
+package com.br.stock.control.model.dto.purchaseOrderItem
+
+import java.math.BigDecimal
+
+data class UpdateOrderItemDTO(
+    var productId: String = "",
+    var quantity: Int = 0,
+    var unitPrice: BigDecimal = BigDecimal.valueOf(0.0),
+    var expectedQuantity: Int = 0,
+    var backOrderedQuantity: Int = 0,
+    var receivedQuantity: Int = 0,
+)
