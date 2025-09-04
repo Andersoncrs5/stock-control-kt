8212182bf073898d67cf61899844e6cb6441a92a
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-04 19:13:32 -0300

    New method findAllByPurchaseOrderId

--- src/main/kotlin/com/br/stock/control/repository/PurchaseOrderItemRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/PurchaseOrderItemRepository.kt
@@ -1,8 +1,13 @@
 package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.PurchaseOrderItem
+import com.br.stock.control.util.filters.purchaseOrderItem.PurchaseOrderItemCustomRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface PurchaseOrderItemRepository: MongoRepository<PurchaseOrderItem, String> {
+interface PurchaseOrderItemRepository: MongoRepository<PurchaseOrderItem, String>, PurchaseOrderItemCustomRepository {
     fun deleteAllByPurchaseOrderId(id: String)
+    fun findAllByPurchaseOrderId(purchaseOrderId: String, pageable: Pageable): Page<PurchaseOrderItem>
+
 }
