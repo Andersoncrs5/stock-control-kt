d0f6fd96ddf37477ccf65545b29350a8de411960
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-07 21:17:17 -0300

    new methods existsByName and findByName

--- src/main/kotlin/com/br/stock/control/repository/RoleRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/RoleRepository.kt
@@ -2,6 +2,10 @@ package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.Role
 import org.springframework.data.mongodb.repository.MongoRepository
+import java.util.Optional
 
 interface RoleRepository: MongoRepository<Role, String> {
+    fun existsByName(name: String): Boolean
+    fun findByName(name: String): Optional<Role>
+
 }
