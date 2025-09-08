1e64f4219d3c04c2490eb6271e5c5ae0ac6d0106
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-08 20:14:59 -0300

    set extend repository custom

--- src/main/kotlin/com/br/stock/control/repository/UserRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/UserRepository.kt
@@ -1,11 +1,12 @@
 package com.br.stock.control.repository
 
 import com.br.stock.control.model.entity.User
+import com.br.stock.control.util.filters.user.UserCustomRepository
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.security.core.userdetails.UserDetails
 import java.util.Optional
 
-interface UserRepository: MongoRepository<User, String> {
+interface UserRepository: MongoRepository<User, String>, UserCustomRepository {
     fun findByEmail(email: String): UserDetails?
     fun existsByEmail(email: String): Boolean
     fun existsByName(email: String): Boolean
