94d196d62ce9d86cc8cc0c54a8508ae9bd067309
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-08 20:12:22 -0300

    removed imports useless

--- src/main/kotlin/com/br/stock/control/config/security/SecurityFilter.kt
+++ src/main/kotlin/com/br/stock/control/config/security/SecurityFilter.kt
@@ -5,8 +5,8 @@ import com.br.stock.control.repository.UserRepository
 import jakarta.servlet.FilterChain
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
