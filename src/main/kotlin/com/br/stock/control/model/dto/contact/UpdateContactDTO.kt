6def83a4cce908555a87fab9ec964d7af3713d78
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-07 21:16:03 -0300

    dto to update a contact

---
+++ src/main/kotlin/com/br/stock/control/model/dto/contact/UpdateContactDTO.kt
@@ -0,0 +1,7 @@
+package com.br.stock.control.model.dto.contact
+
+data class UpdateContactDTO(
+    var secondaryEmail: String? = null,
+    var phone: String? = null,
+    var secondaryPhone: String? = null,
+)
