2f39b654ae013ed780c2e9d69b934ac24cb981a9
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-08 20:14:44 -0300

    removed {}

--- src/main/kotlin/com/br/stock/control/repository/SystemLogRepository.kt
+++ src/main/kotlin/com/br/stock/control/repository/SystemLogRepository.kt
@@ -3,5 +3,4 @@ package com.br.stock.control.repository
 import com.br.stock.control.model.entity.SystemLog
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface SystemLogRepository: MongoRepository<SystemLog, String> {
-}
+interface SystemLogRepository: MongoRepository<SystemLog, String>
