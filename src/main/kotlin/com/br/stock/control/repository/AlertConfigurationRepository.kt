6b5aea80a2921082fe46455f20194bb44c5e8fa9
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-09 20:58:44 -0300

    removed AlertConfigurationRepository

--- src/main/kotlin/com/br/stock/control/repository/AlertConfigurationRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.br.stock.control.repository
-
-import com.br.stock.control.model.entity.AlertConfiguration
-import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface AlertConfigurationRepository : MongoRepository<AlertConfiguration, String>
